2d5ad7ade11e9d3353962a6759c6832e4b034835
Yorachukwu <yorajideonwo@gmail.com>
2026-08-05 15:59:19 +0100

    Update animal.py

--- Src/animal.py
+++ Src/animal.py
@@ -1,5 +1,17 @@
+import pygame
+import tile
 class Animal():
+
+    is_alive = True
+
     def __init__(self, health, speed, food):
         self.health = health
         self.speed = speed
         self.food = food
+
+
+    def Move(self):
+        is_alive = True
+        clock = pygame.time.wait(3000)
+
+
